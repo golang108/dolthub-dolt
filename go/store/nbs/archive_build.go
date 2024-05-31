afb374c6de48ad56d60f55ce1189b39d406548db
Neil Macneale IV <neil@dolthub.com>
2024-05-31 09:13:50 -0700

    Only swap tables once to avoid stale file handles

--- go/store/nbs/archive_build.go
+++ go/store/nbs/archive_build.go
@@ -23,7 +23,6 @@ import (
 	"os"
 	"sort"
 	"sync"
-	"time"
 
 	"github.com/dolthub/gozstd"
 	"golang.org/x/sync/errgroup"
@@ -39,8 +38,9 @@ func BuildArchive(ctx context.Context, cs chunks.ChunkStore, dagGroups *ChunkRel
 		outPath, _ := gs.oldGen.Path()
 		oldgen := gs.oldGen.tables.upstream
 
+		swapMap := make(map[hash.Hash]hash.Hash)
+
 		for tf, ogcs := range oldgen {
-			altCtx, _ := context.WithTimeout(ctx, 1000*time.Minute)
 			// NM4 - We should probably provide a way to pick a particular table file to build an archive for.
 
 			idx, err := ogcs.index()
@@ -50,7 +50,7 @@ func BuildArchive(ctx context.Context, cs chunks.ChunkStore, dagGroups *ChunkRel
 
 			archivePath := ""
 			archiveName := hash.Hash{}
-			archivePath, archiveName, err = convertTableFileToArchive(altCtx, ogcs, idx, dagGroups, outPath)
+			archivePath, archiveName, err = convertTableFileToArchive(ctx, ogcs, idx, dagGroups, outPath)
 			if err != nil {
 				return err
 			}
@@ -60,23 +60,24 @@ func BuildArchive(ctx context.Context, cs chunks.ChunkStore, dagGroups *ChunkRel
 				return err
 			}
 
-			// p("Verified table file: %s\n", archivePath)
+			swapMap[tf] = archiveName
+		}
 
-			//NM4 TODO: This code path must only be run on an offline database. We should add a check for that.
-			specs, err := gs.oldGen.tables.toSpecs()
-			newSpecs := make([]tableSpec, 0, len(specs))
-			for _, spec := range specs {
-				if spec.name != tf {
-					newSpecs = append(newSpecs, spec)
-				} else {
-					newSpecs = append(newSpecs, tableSpec{archiveName, spec.chunkCount})
-				}
-			}
-			err = gs.oldGen.swapTables(altCtx, newSpecs)
-			if err != nil {
-				return err
+		//NM4 TODO: This code path must only be run on an offline database. We should add a check for that.
+		specs, err := gs.oldGen.tables.toSpecs()
+		newSpecs := make([]tableSpec, 0, len(specs))
+		for _, spec := range specs {
+			if newSpec, exists := swapMap[spec.name]; exists {
+				newSpecs = append(newSpecs, tableSpec{newSpec, spec.chunkCount})
+			} else {
+				newSpecs = append(newSpecs, spec)
 			}
 		}
+		err = gs.oldGen.swapTables(ctx, newSpecs)
+		if err != nil {
+			return err
+		}
+
 	} else {
 		return errors.New("Modern DB Expected")
 	}
