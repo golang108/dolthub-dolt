34bf578860f8157e1ecbcc66a1d26b5259302490
Neil Macneale IV <neil@dolthub.com>
2024-06-17 11:16:34 -0700

    and now a single thread

--- go/store/nbs/archive_build.go
+++ go/store/nbs/archive_build.go
@@ -568,7 +568,7 @@ func (cr *ChunkRelations) convertToChunkGroups(ctx context.Context, chks *Simple
 	resultChannel := make(chan *chunkGroup, len(groups))
 
 	// Start worker goroutines
-	numThreads := 2
+	numThreads := 1
 	var wg sync.WaitGroup
 	wg.Add(numThreads)
 	for i := 0; i < numThreads; i++ {
